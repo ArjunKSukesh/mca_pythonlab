95529bb34854fdc69bda914c354098f8f5bb9c78
Arju999 <58620689+Arju999@users.noreply.github.com>
2021-11-03 11:45:26 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+print("a ="+str(a))
+print("b ="+str(b))
+a,b=b,a
+print("a ="+str(a))
+print("b ="+str(b))
---
+++ temp.py
@@ -0,0 +1,3 @@
+c=int(input("enter the temperature :"))
+t=c*1.8*32
+print("temperature = "+str(t))
