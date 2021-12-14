76d1516e4217b1ab0a5b481520d18fb048d7d0ba
Arjun k <58620689+Arju999@users.noreply.github.com>
2021-12-15 01:01:53 +0530

    Update 5-listcompare.py

--- 5-listcompare.py
+++ 5-listcompare.py
@@ -1,39 +1,23 @@
-list1=[]
-list2=[]
-
-a=int(input("Enter the number limit of list 1 :"))
-for i in range(0,a):
- b=int(input("Enter the numbers :"))
- list1.append(b)
-print(list1)
-
-c=int(input("Enter the number limit of list 2 :"))
-for j in range(0,c):
- d=int(input("Enter the numbers :"))
- list2.append(d)
-print(list2)
-
-list1_len=len(list1)
-print("Lenght of the list 1 :",list1_len)
-
-list2_len=len(list2)
-print("Lenght of the list 2:",list2_len)
-
-if list1_len==list2_len:
-  print("Lenght of two list are same")
+count=0
+a=list(map(int,input("enter 1st list of numbers seperated by comas : ").split(' ')));
+b=list(map(int,input("enter 2nd list of numbers seperated by comas : ").split(' ')));
+if len(a)==len(b):
+    print("both list is of same length....")
 else:
-  print("Lenght of two list are not same")
+    print("both list are of different length.....")
 
-print("Sum of list 1 =",sum(list1))
-print("Sum of list 2 =",sum(list2))
 
-if sum(list1)==sum(list2):
-  print("Sum of two list are same")
+if sum(a)==sum(b):
+    print("sum of both list is equal....")
 else:
-  print("Sum of two list are not same")
-for i in list1:
- for j in list2:
-   if i==j:
-     print("Element is found in both",i)
+    print("sum of both list is not equal....")
+
+for i in range(0,len(a)):
+    for j in range(0,len(b)):
+        if a[i]==b[j]:
+            print(a[i]," occure in both")
+            count+=1
+if count==0:
+    print("no values occure in both lists")
 
 
